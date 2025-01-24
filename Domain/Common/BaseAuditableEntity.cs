7f20e7735b179915562ae74fef356bd0d3d37732
Dima Paiuk <dimapajuk2325@gmail.com>
2025-01-24 18:09:59 +0200

    Made CreatedBy not nullable

--- Domain/Common/BaseAuditableEntity.cs
+++ Domain/Common/BaseAuditableEntity.cs
@@ -3,7 +3,7 @@ namespace Domain.Common;
 public abstract class BaseAuditableEntity : BaseEntity
 {
     public DateTimeOffset CreatedAtUtc { get; set; }
-    public string? CreatedBy { get; set; }
+    public string CreatedBy { get; set; } = null!;
 
     public DateTimeOffset? LastModifiedAtUtc { get; set; }
     public string? LastModifiedBy { get; set; }
