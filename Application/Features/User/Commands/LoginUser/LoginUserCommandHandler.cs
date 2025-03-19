195f2ef2145db5f70434e069fff97948a1b25e2a
Dima Paiuk <dimapajuk2325@gmail.com>
2025-03-19 22:23:21 +0200

    Refactor LoginUser command and handler to return Result<string> for improved error handling

--- Application/Features/User/Commands/LoginUser/LoginUserCommandHandler.cs
+++ Application/Features/User/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -1,3 +1,4 @@
+using Application.Common.Abstraction;
 using Application.Common.Interfaces.Auth;
 using Application.Common.Interfaces.Persistence.Repositories;
 using MediatR;
@@ -8,9 +9,9 @@ public class LoginUserCommandHandler(
     IUserRepository userRepository,
     IPasswordHasher passwordHasher,
     IJwtTokenProvider jwtTokenProvider)
-    : IRequestHandler<LoginUserCommand, string>
+    : IRequestHandler<LoginUserCommand, Result<string>>
 {
-    public async Task<string> Handle(
+    public async Task<Result<string>> Handle(
         LoginUserCommand request, 
         CancellationToken cancellationToken)
     {
@@ -18,16 +19,18 @@ public class LoginUserCommandHandler(
 
         if (user is null)
         {
-            throw new UnauthorizedAccessException("User with the provided email does not exist.");
+            return Result<string>.Failure("User with provided email does not exist.");
         }
 
         var passwordVerificationResult = passwordHasher.Verify(request.Password, user.PasswordHash);
         
         if (!passwordVerificationResult)
         {
-            throw new UnauthorizedAccessException("Invalid credentials provided.");
+            return Result<string>.Failure("Invalid credentials provided.");
         }
 
-        return await jwtTokenProvider.GenerateTokenAsync(user);
+        var token = await jwtTokenProvider.GenerateTokenAsync(user);
+        
+        return Result<string>.Success(token);
     }
 }
